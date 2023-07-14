ffc448ff0501ad4efad2a3e1ec44c2f64709413b
vinivin153 <vinivin153@naver.com>
2023-07-14 19:42:10 +0900

    [SP-153] feat: 성별 버튼 디자인 적용

--- src/pages/SignUp/SignUpNickName.tsx
+++ src/pages/SignUp/SignUpNickName.tsx
@@ -1,6 +1,7 @@
 import { Input } from 'components/Input';
 import { Button } from 'components/Button';
 import * as S from './style';
+import { useState } from 'react';
 
 interface Props {
   userInfo: {
@@ -11,19 +12,34 @@ interface Props {
     userPhoneNumber: string;
   };
   handleNextClick(): void;
-  handleButtonClick(e: React.MouseEvent<HTMLButtonElement>): void;
+  setUserGender(usergender: string): void;
   handleUserInfoChange(e: React.FormEvent<HTMLInputElement>): void;
 }
+const genderList = ['남성', '여성'];
 
-export const SignUpNickName = ({ userInfo, handleUserInfoChange, handleButtonClick, handleNextClick }: Props) => {
+export const SignUpNickName = ({ userInfo, handleUserInfoChange, setUserGender, handleNextClick }: Props) => {
+  const [selectedGender, setSelectedGender] = useState('');
+
+  const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setUserGender(e.currentTarget.innerText);
+    setSelectedGender(e.currentTarget.innerText);
+  };
   return (
     <>
       <Input title="닉네임" value={userInfo.userNickName} name="userNickName" onChange={handleUserInfoChange} />
       <S.GenderWrapper>
         <span>성별</span>
         <S.ButtonWrapper>
-          <Button title="남성" size="medium" color="#999" background="#fafafa" onClick={handleButtonClick}></Button>
-          <Button title="여성" size="medium" color="#999" background="#fafafa" onClick={handleButtonClick}></Button>
+          {genderList.map((gender) => (
+            <Button
+              key={gender}
+              title={gender}
+              size="medium"
+              color={selectedGender === gender ? '#FF5680' : '#999'}
+              background={selectedGender === gender ? 'rgba(255, 86, 128, 0.10)' : '#F2F2F2'}
+              onClick={handleButtonClick}
+            ></Button>
+          ))}
         </S.ButtonWrapper>
       </S.GenderWrapper>
       <Button title="다음" onClick={handleNextClick}></Button>
