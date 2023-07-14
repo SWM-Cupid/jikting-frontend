4f72ee04cd42af5794a170ed073ef83991aecec8
vinivin153 <vinivin153@naver.com>
2023-07-14 11:18:40 +0900

    [SP-153] design: 성별 버튼 위치 조정

--- src/pages/SignUp/style.tsx
+++ src/pages/SignUp/style.tsx
@@ -12,3 +12,14 @@ export const Content = styled.div`
   gap: 2.6rem;
   margin: 2rem auto;
 `;
+
+export const GenderWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin-top: 1rem;
+`;
