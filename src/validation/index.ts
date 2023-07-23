1832738fd05ccc2589fd6414edceb2ff14d8889a
vinivin153 <vinivin153@naver.com>
2023-07-23 21:27:36 +0900

    [SP-171] feat: 유효성 검사 기능 추가

---
+++ src/validation/index.ts
@@ -0,0 +1,24 @@
+export const validIdCheck = (input: string) => {
+  const validIdPattern = /^[a-z]{1}[a-z0-9]{5,19}$/;
+  return validIdPattern.test(input) ? true : '6-20자의 영문 소문자, 숫자를 입력해주세요';
+};
+
+export const validPasswordCheck = (input: string) => {
+  const validPasswordPattern = /^[a-zA-Z0-9!@#$%^&*]{10,}$/;
+  return validPasswordPattern.test(input) ? true : '10자이상 영문 대/소문자,숫자,특수문자를 입력해주세요';
+};
+
+export const validNickNameCheck = (input: string) => {
+  const validNickNamePattern = /^[ㄱ-힣a-zA-Z]{2,15}$/;
+  return validNickNamePattern.test(input) ? true : '2-15자의 한글, 소문자, 대문자를 입력해주세요';
+};
+
+export const validNameCheck = (input: string) => {
+  const validNamePattern = /^[ㄱ-힣]{2,6}$/;
+  return validNamePattern.test(input) ? true : '2-6자의 한글을 입력해주세요';
+};
+
+export const validPhoneNumberCheck = (input: string) => {
+  const validPhoneNumberPattern = /^[0-9]{11}$/;
+  return validPhoneNumberPattern.test(input) ? true : '올바른 휴대폰 번호를 입력해주세요';
+};
