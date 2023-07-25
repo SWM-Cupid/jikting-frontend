09cd24b2504bb7f5ca5847b762d2f5ec45217cce
vinivin153 <vinivin153@naver.com>
2023-07-25 19:11:28 +0900

    [SP-206] feat: TextArea forwardRef 적용

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -1,10 +1,12 @@
+import { forwardRef } from 'react';
 import * as S from './style';
 
-export const TextArea = ({ title }: { title: string }) => {
+export const TextArea = forwardRef(({ title }: { title: string }, ref: React.Ref<HTMLTextAreaElement> | null) => {
   return (
     <S.TextAreaWrapper>
       <S.Title>{title}</S.Title>
-      <S.TextArea placeholder="한줄소개를 입력해주세요."></S.TextArea>
+      <S.TextArea placeholder="한줄소개를 입력해주세요." ref={ref}></S.TextArea>
     </S.TextAreaWrapper>
   );
-};
+});
+TextArea.displayName = 'TextArea';
