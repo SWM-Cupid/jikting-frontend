b8330b26932abc4d6b91c1271f2b2fcd1b9d05ba
vinivin153 <vinivin153@naver.com>
2023-07-27 13:47:09 +0900

    [SP-173] chore: proxy 서버 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,12 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://jikting.com:8080',
+      },
+    },
+  },
   plugins: [react(), tsconfigPaths(), svgr({ exportAsDefault: true })],
 });
