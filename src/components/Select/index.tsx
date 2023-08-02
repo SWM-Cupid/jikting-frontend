38a347778a7184725da788728f2c50f9ea584d5a
vinivin153 <vinivin153@naver.com>
2023-08-02 15:17:31 +0900

    [SP-217] design: 단위 수정 및 타이틀 디자인 적용

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,5 +1,5 @@
 import { ComponentProps, forwardRef } from 'react';
-import { FlexWrapper, StyledSelect } from './style';
+import { FlexWrapper, StyledSelect, Title } from './style';
 
 interface SelectProps extends ComponentProps<'select'> {
   optionList: string[];
@@ -9,7 +9,7 @@ export const Select = forwardRef(
   ({ optionList, title, defaultValue, onChange }: SelectProps, ref: React.Ref<HTMLSelectElement> | null) => {
     return (
       <FlexWrapper>
-        <span>{title}</span>
+        <Title>{title}</Title>
         <StyledSelect onChange={onChange} defaultValue={defaultValue} ref={ref}>
           {optionList.map((item) => {
             return <option key={item}>{item}</option>;
