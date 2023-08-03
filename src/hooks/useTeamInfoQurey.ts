bc6b5a54eec3c35bcfcb01af0f69f30f0b4c038b
vinivin153 <vinivin153@naver.com>
2023-08-03 19:41:54 +0900

    [SP-221] feat: 팀 만들기 API 연동

--- src/hooks/useTeamInfoQurey.ts
+++ src/hooks/useTeamInfoQurey.ts
@@ -1,14 +1,25 @@
-import { fetchTeamInfo } from 'api/team';
-import { useQuery } from 'react-query';
+import { fetchCreateTeam, fetchTeamInfo } from 'api/team';
+import { useMutation, useQuery } from 'react-query';
 
 export const useQueryTeamInfo = () => {
   return useQuery(['teamInfo'], fetchTeamInfo, {
-    onSuccess: (data) => {
+    onSuccess: ({ data }) => {
       console.log(data);
     },
     onError: (error) => {
       console.log(error);
     },
     retry: false,
+  });
+};
+
+export const useMutateCreateTeam = () => {
+  return useMutation(fetchCreateTeam, {
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
   });
 };
