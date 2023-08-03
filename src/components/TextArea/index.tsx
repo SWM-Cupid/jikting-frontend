393f0ed3e182a1d69bee883621ee2d9524109c16
vinivin153 <vinivin153@naver.com>
2023-08-03 19:40:36 +0900

    [SP-221] feat: props 추가

--- src/components/TextArea/index.tsx
+++ src/components/TextArea/index.tsx
@@ -1,11 +1,14 @@
-import { forwardRef } from 'react';
+import { ComponentProps, forwardRef } from 'react';
 import * as S from './style';
 
-export const TextArea = forwardRef(({ title }: { title: string }, ref: React.Ref<HTMLTextAreaElement> | null) => {
+interface Props extends ComponentProps<'textarea'> {
+  title: string;
+}
+export const TextArea = forwardRef(({ title, ...props }: Props, ref: React.Ref<HTMLTextAreaElement> | null) => {
   return (
     <S.TextAreaWrapper>
       <S.Title>{title}</S.Title>
-      <S.TextArea placeholder="한줄소개를 입력해주세요." ref={ref}></S.TextArea>
+      <S.TextArea placeholder="한줄소개를 입력해주세요." {...props} ref={ref}></S.TextArea>
     </S.TextAreaWrapper>
   );
 });
