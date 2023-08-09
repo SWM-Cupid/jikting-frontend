bd1b1c3ef871bc1075608fc95c8150a9a80870e6
vinivin153 <vinivin153@naver.com>
2023-08-09 13:50:40 +0900

    [SP-207] feat: 채팅방 API 연동

--- src/hooks/useChatQuery.ts
+++ src/hooks/useChatQuery.ts
@@ -1,8 +1,19 @@
-import { fetchChatList } from 'api/chat';
+import { fetchChatList, fetchChatRoom } from 'api/chat';
 import { useQuery } from 'react-query';
 
 export const useChatListQuery = () => {
   return useQuery(['chatlist'], fetchChatList, {
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+};
+
+export const useChatRoomQuery = (chattingRoomId: number) => {
+  return useQuery(['chatroom'], () => fetchChatRoom(chattingRoomId), {
     onSuccess: (data) => {
       console.log(data);
     },
