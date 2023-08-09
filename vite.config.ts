02f057044c456f4799ed3614e6bc2d4568aa942a
vinivin153 <vinivin153@naver.com>
2023-08-09 13:37:39 +0900

    [SP-207] chore: 프록시 설정 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,5 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://jikting.com:8080',
-      },
-    },
-  },
   plugins: [react(), tsconfigPaths(), svgr({ exportAsDefault: true })],
 });
