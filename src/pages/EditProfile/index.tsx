27d7fd3de164cea2bc8e45cb4ade7b6ca191bb90
vinivin153 <vinivin153@naver.com>
2023-08-13 13:25:31 +0900

    [SP-282] feat: 나이 -> 생년월일로 변경 및 흡연여부 타입 변경

--- src/pages/EditProfile/index.tsx
+++ src/pages/EditProfile/index.tsx
@@ -1,14 +1,15 @@
 import { Header } from 'components/Header';
 import * as S from './style';
 import { UploadImage } from 'components/UploadImage';
-import { Input } from 'components/Input';
 import { Select } from 'components/Select';
 import { Keyword } from 'components/Keyword';
 import { TextArea } from 'components/TextArea';
 import { Button } from 'components/Button';
 import { theme } from 'styles/theme';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { useState } from 'react';
+import { validBirthCheck } from 'validation';
+import { Input } from 'components/Input';
 
 const MBTI_LIST = [
   'INTJ',
@@ -28,17 +29,22 @@ const MBTI_LIST = [
   'ESTP',
   'ESFP',
 ];
-const IS_DRINKING_LIST = ['마시면 꽐라', '적당히 마심', '조금 마심', '금주'];
-const IS_SMOCKING_LIST = ['흡연', '비흡연'];
+const DRINKING_OPTION_List = ['자주 마심', '적당히 마심', '조금 마심', '금주'];
+const SMOCKING_OPTION_LIST = ['흡연', '비흡연'];
+const PERSONALITY_LIST = ['다정한', '활발한', '유머러스한', '내성적인', '외향적인'];
+const HOBBY_LIST = ['등산', '클라이밍', '게임', '러닝', '영화감상', '음악감상', '여행'];
 
 export const EditProfile = () => {
-  const { register, handleSubmit } = useForm<IForm>();
-  const { setValue } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
   const [personalities, setPersonalities] = useState<string[]>([]);
   const [hobbies, setHobbies] = useState<string[]>([]);
 
   interface IForm {
-    age: number;
+    age: string;
     height: number;
     mbti: string;
     address: string;
@@ -49,14 +55,16 @@ export const EditProfile = () => {
     personalities: string[];
     hobbies: string[];
     images: { url: string; sequence: string }[];
-    smoke: boolean;
+    smoke: string;
   }
 
-  const onSubmit = (data: IForm) => {
-    data.age = Number(data.age);
+  const onSubmit: SubmitHandler<IForm> = (data: IForm) => {
+    const age = data.age;
+    data.age = `${age.slice(0, 4)}.${age.slice(4, 6)}.${age.slice(6)}`;
     data.height = Number(data.height);
     data['hobbies'] = hobbies;
     data['personalities'] = personalities;
+    console.log(data);
   };
 
   const getPersonalities = (selectedKeywords: string[]) => {
@@ -73,32 +81,35 @@ export const EditProfile = () => {
       <S.UploadImageWrapper>
         <UploadImage size="medium" />
       </S.UploadImageWrapper>
-      <Input title="나이" {...register('age')} />
-      <Input title="키" {...register('height')} />
-      <Input title="거주지" {...register('address')} />
-      <Select title="MBTI" optionList={MBTI_LIST} {...register('mbti')} />
-      <Select title="음주여부" optionList={IS_DRINKING_LIST} {...register('drinkStatus')} />
-      <Select
-        title="흡연여부"
-        optionList={IS_SMOCKING_LIST}
-        {...register('smoke')}
-        onChange={() => {
-          setValue('흡연', true), setValue('비흡연', false);
-        }}
+      <Input
+        title="생년월일"
+        error={errors.age}
+        placeholder="ex> 19990130"
+        {...register('age', {
+          required: '생년월일 입력은 필수 입니다.',
+          validate: {
+            validBirthCheck,
+          },
+        })}
       />
+      <Input title="키(cm)" error={errors.height} {...register('height', { required: '키 입력은 필수 입니다.' })} />
+      <Input
+        title="거주지"
+        error={errors.address}
+        placeholder="ex> 서울시 광진구"
+        {...register('address', { required: '거주지 입력은 필수 입니다.' })}
+      />
+      <Select title="MBTI" optionList={MBTI_LIST} {...register('mbti', { required: true })} />
+      <Select title="음주여부" optionList={DRINKING_OPTION_List} {...register('drinkStatus', { required: true })} />
+      <Select title="흡연여부" optionList={SMOCKING_OPTION_LIST} {...register('smoke', { required: true })} />
       <Input title="출신대학교(선택)" {...register('college')} />
       <Keyword
         title="성격"
         defaultKeywordList={personalities}
-        keywordList={['가나', '다라마', '바사아', '자차카']}
+        keywordList={PERSONALITY_LIST}
         getKeywordList={getPersonalities}
       />
-      <Keyword
-        title="취미"
-        defaultKeywordList={hobbies}
-        keywordList={['운동', '헬스', '등산', '개발']}
-        getKeywordList={getHobbies}
-      />
+      <Keyword title="취미" defaultKeywordList={hobbies} keywordList={HOBBY_LIST} getKeywordList={getHobbies} />
       <TextArea title="한줄 소개(선택)" {...register('description')} />
       <Button title="수정 완료" type="submit" size="large" background={theme.colors.mainPink} color="white" />
     </S.EditProfileForm>
