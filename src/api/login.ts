9714f3371c3f9d784741e7b598b05077b8598eeb
vinivin153 <vinivin153@naver.com>
2023-08-22 10:14:14 +0900

    [SP-170] feat: 유저 pk값 저장

--- src/api/login.ts
+++ src/api/login.ts
@@ -9,7 +9,9 @@ export const fetchLogin = async ({ username, password }: Props) => {
   const data = await unauthenticated.post('/members/login', { username, password });
   const accessToken = data.headers['authorization'];
   const refreshToken = data.headers['authorization-refresh'];
+  const userId = data.headers['memberprofileid'];
 
   localStorage.setItem('actk', `Bearer ${accessToken}`);
+  localStorage.setItem('uid', userId);
   sessionStorage.setItem('rftk', refreshToken);
 };
