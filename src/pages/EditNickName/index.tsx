9bfb43a8530f20fc09c5f9ce51189ad11e28b202
vinivin153 <vinivin153@naver.com>
2023-09-04 15:51:53 +0900

    [SP-368] feat: 닉네임 수정 중복검사 기능 구현

--- src/pages/EditNickName/index.tsx
+++ src/pages/EditNickName/index.tsx
@@ -3,11 +3,35 @@ import { ButtonWrapper, EditNickNameWrapper } from './style';
 import { Input } from 'components/Input';
 import { Button } from 'components/Button';
 import { theme } from 'styles/theme';
-export const EditNickName = () => {
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { validNickNameCheck } from 'validation';
+import { fetchNickNameCheck } from 'api/signup';
+
+interface NickName {
+  nickname: string;
+}
+
+export const EditNickName = ({ nickname }: NickName) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<NickName>({ mode: 'onTouched' });
+
+  const onSubmit: SubmitHandler<NickName> = (data: NickName) => {
+    console.log(data);
+  };
   return (
-    <EditNickNameWrapper>
+    <EditNickNameWrapper onSubmit={handleSubmit(onSubmit)}>
       <Header previous title="닉네임" />
-      <Input />
+      <Input
+        error={errors.nickname}
+        {...register('nickname', {
+          value: nickname,
+          required: true,
+          validate: { validNickNameCheck, fetchNickNameCheck },
+        })}
+      />
       <ButtonWrapper>
         <Button type="submit" background={theme.colors.mainPink} color="White" title="변경하기" />
       </ButtonWrapper>
