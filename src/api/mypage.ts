d0c62e06fff8c6af2b7f1f57a379f065e7ea857e
vinivin153 <vinivin153@naver.com>
2023-09-08 21:27:55 +0900

    [SP-394] feat: 사용자 정보 API 연동

--- src/api/mypage.ts
+++ src/api/mypage.ts
@@ -6,6 +6,25 @@ interface MyInfo {
   company: string;
   imageUrl: string;
 }
+interface Images {
+  url: string;
+  sequence: string;
+}
+
+interface MyProfileInfo {
+  birth: string;
+  height: number;
+  gender: string;
+  address: string;
+  mbti: string;
+  smokeStatus: string;
+  drinkStatus: string;
+  college: string;
+  personalities: string[];
+  hobbies: string[];
+  description: string;
+  images: Images[];
+}
 
 export const fetchMyInfo = async () => {
   const response: AxiosResponse<MyInfo> = await authenticated.get('/members');
@@ -22,4 +41,9 @@ export const fetchEditProfile = async (data: FormData) => {
 
 export const fetchEditNickName = async (data: { nickname: string }) => {
   return await authenticated.patch('/members', data);
+};
+
+export const fetchMyProfileInfo = async () => {
+  const response: AxiosResponse<MyProfileInfo> = await authenticated.get('/members/profile');
+  return response;
 };
--- src/hooks/useMypageQuery.ts
+++ src/hooks/useMypageQuery.ts
@@ -1,7 +1,12 @@
-import { fetchMyInfo } from 'api/mypage';
+import { fetchMyInfo, fetchMyProfileInfo } from 'api/mypage';
 import { useQuery } from 'react-query';
 
 export const useQueryMyInfo = () => {
   const { data } = useQuery(['myInfo'], fetchMyInfo);
+  return data?.data;
+};
+
+export const useQueryMyProfileInfo = () => {
+  const { data } = useQuery(['myProfileInfo'], fetchMyProfileInfo);
   return data?.data;
 };
