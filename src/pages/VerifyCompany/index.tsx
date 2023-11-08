55a678d3e3e5a1972a3af0bea4dcd53e74d2b125
vinivin153 <vinivin153@naver.com>
2023-11-08 20:52:11 +0900

    [SP-401] feat: 회사 이메일 인증 API연동

--- src/pages/VerifyCompany/index.tsx
+++ src/pages/VerifyCompany/index.tsx
@@ -6,50 +6,60 @@ import { theme } from 'styles/theme';
 import { validEmailCheck } from 'validation';
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
+import { fetchSendEmailCode, fetchVerifyEmailCode } from 'api/mypage';
+import { useNavigate } from 'react-router-dom';
 
 interface VerfiyForm {
   email: string;
-  verifyCode: number;
+  verificationCode: number;
 }
 
 export const VerifyCompany = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid, isDirty },
+    setError,
+    formState: { errors, isValid },
   } = useForm<VerfiyForm>();
   const [isSendCode, setIsSendCode] = useState(false);
+  const navigate = useNavigate();
 
-  const onSubmit = (formData: VerfiyForm) => {
+  const onSubmit = async (formData: VerfiyForm) => {
     if (!isSendCode) {
-      // TODO: api연동
+      fetchSendEmailCode(formData.email);
       setIsSendCode(true);
+      return;
+    }
+
+    if (await fetchVerifyEmailCode(formData.email, formData.verificationCode)) {
+      alert('회사인증 완료');
+      navigate('/mypage');
+    } else {
+      setError('verificationCode', { type: 'value', message: '인증번호가 일치하지 않습니다.' });
     }
   };
 
   return (
     <VerifyEmailWrapper onSubmit={handleSubmit(onSubmit)}>
       <Header title="회사 인증" previous />
-      {isSendCode && <Input title="인증번호" {...register('verifyCode', { required: true })}></Input>}
+      {isSendCode && (
+        <Input
+          title="인증번호"
+          error={errors.verificationCode}
+          {...register('verificationCode', { required: true })}
+        ></Input>
+      )}
       <Input
         title="회사 이메일"
         error={errors.email}
         {...register('email', { required: true, validate: validEmailCheck })}
       ></Input>
 
-      {isSendCode ? (
-        <Button
-          title="인증완료"
-          onClick={() => alert('ho')}
-          background={isValid ? theme.colors.mainPink : theme.colors.buttonGrey}
-        ></Button>
-      ) : (
-        <Button
-          type="submit"
-          title="인증하기"
-          background={isValid ? theme.colors.mainPink : theme.colors.buttonGrey}
-        ></Button>
-      )}
+      <Button
+        type="submit"
+        title={isSendCode ? '인증하기' : '인증완료'}
+        background={isValid ? theme.colors.mainPink : theme.colors.buttonGrey}
+      ></Button>
     </VerifyEmailWrapper>
   );
 };
