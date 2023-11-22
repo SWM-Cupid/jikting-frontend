d3b93efb42bb4864b7ec2cb2d2ff793319d9b929
vinivin153 <vinivin153@naver.com>
2023-11-22 20:32:44 +0900

    [SP-430] feat: 팀 참여 버튼 생성

--- src/pages/Team/index.tsx
+++ src/pages/Team/index.tsx
@@ -8,10 +8,15 @@ import { CreateTeam } from './CreateTeam';
 import { useQueryTeamInfo } from 'hooks/useTeamInfoQurey';
 import { TeamDetail } from './TeamDetail';
 import { EmptyData } from 'components/EmptyData';
+import ModalPortal from 'components/Modal/ModalPortal';
+import { ModalBottomSheet } from 'components/Modal';
+import { Input } from 'components/Input';
 
 export const TeamList = () => {
   const { isSuccess, isError, data } = useQueryTeamInfo();
   const [registerTeam, setRegisterTeam] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [url, setUrl] = useState('');
 
   const handleRegisterTeamClick = useCallback(() => {
     setRegisterTeam(true);
@@ -20,6 +25,19 @@ export const TeamList = () => {
   const handleRegisterTeamCompleteClick = useCallback(() => {
     setRegisterTeam(false);
   }, []);
+
+  const hanleModalOpenClick = () => {
+    setModalOpen(true);
+  };
+
+  const handelModalCloseClick = () => {
+    setModalOpen(false);
+  };
+
+  const handleAttendTeamClick = (e: React.FormEvent) => {
+    e.preventDefault();
+    window.location.href = url;
+  };
 
   if (registerTeam) return <CreateTeam handleRegisterTeamCompleteClick={handleRegisterTeamCompleteClick} />;
 
@@ -35,14 +53,31 @@ export const TeamList = () => {
           <NavBar defaultActive="team" />
           <S.ButtonWrapper>
             <Button
-              size="large"
+              size="medium"
               title="팀 등록하기"
               background="#FFEEF2"
               color={theme.colors.mainPink}
               onClick={handleRegisterTeamClick}
             ></Button>
+            <Button
+              size="medium"
+              title="팀 참여하기"
+              background="#FFEEF2"
+              color={theme.colors.mainPink}
+              onClick={hanleModalOpenClick}
+            ></Button>
           </S.ButtonWrapper>
         </S.TeamWrapper>
+        {modalOpen ? (
+          <ModalPortal>
+            <ModalBottomSheet handleMaskClick={handelModalCloseClick}>
+              <S.Form onSubmit={handleAttendTeamClick}>
+                <Input title="초대 링크를 입력해주세요" value={url} onChange={(e) => setUrl(e.target.value)}></Input>
+                <Button type="submit" title="완료" background={theme.colors.mainPink}></Button>
+              </S.Form>
+            </ModalBottomSheet>
+          </ModalPortal>
+        ) : null}
       </>
     );
 
